b6ad6686760ad2c90bd91a9f73e0bf22e052ae02
a-min-salehi <aminesalehi80@outlook.com>
2024-01-19 11:10:01 +0330

    beginer-level 01_07 to 01_09 added

---
+++ beginer-level/01_07shift.py
@@ -0,0 +1,3 @@
+x = int(input("enter x :"))
+y = ( x << 4 ) + x + 15
+print("y = ",y)
---
+++ beginer-level/01_09change_x1andx2.py
@@ -0,0 +1,6 @@
+x1 = eval(input("enter x1: "))
+x2 = eval(input("enter x2: "))
+
+x1,x2 = x2,x1
+
+print("x1 =", x1 , "x2 =", x2)
